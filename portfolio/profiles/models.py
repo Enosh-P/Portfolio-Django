cbfecb4ecd0b90929041e6bd286564c5e383de52
Enosh <enoshponraj@gmail.com>
2026-04-28 09:07:47 +0200

    use direct assignment if upload doesnt work

--- portfolio/profiles/models.py
+++ portfolio/profiles/models.py
@@ -132,14 +132,9 @@ class Profile(models.Model):
         field_name = f'cv_file_{language}'
         cv_file = getattr(self, field_name, None)
         if language == 'de':
-            self.cv_file_de = "cv/Enosh_lebenslauf.pdf"
-            return self.cv_file_de
-        if language == 'en':
+            cv_file.name = "cv/Enosh_lebenslauf.pdf"
+        elif not self.cv_file_en:
             self.cv_file_en = "cv/Enosh_CV.pdf"
-            return self.cv_file_en
-        if language == 'ta':
-            self.cv_file_ta = "cv/Enosh_CV.pdf"
-            return self.cv_file_ta
         return cv_file or self.cv_file_en
 
 
